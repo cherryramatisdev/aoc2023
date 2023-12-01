9ffeddca4a50819f693683731f4bd7e4fb901bb5
Cherry Ramatis <cherry.ramatis@icloud.com>
2023-12-01 19:51:31 -0300

    feat: add first day

---
+++ lib/first/first.ex
@@ -0,0 +1,37 @@
+defmodule Aoc2023.First do
+  def part1 do
+    String.split(get_challenge(), "\n", trim: true)
+    |> Enum.map(fn x -> String.graphemes(x) |> Enum.filter(&Aoc2023.First.is_numeric?/1) end)
+    |> Enum.map(fn x -> String.to_integer("#{List.first(x)}#{List.last(x)}") end)
+    |> Enum.reduce(&(&1 + &2))
+  end
+
+  def part2 do
+    String.split(get_challenge(), "\n", trim: true)
+    |> Enum.map(fn x ->
+      x
+      |> String.replace("one", "one1one")
+      |> String.replace("two", "two2two")
+      |> String.replace("three", "three3three")
+      |> String.replace("four", "four4four")
+      |> String.replace("five", "five5five")
+      |> String.replace("six", "six6six")
+      |> String.replace("seven", "seven7seven")
+      |> String.replace("eight", "eight8eight")
+      |> String.replace("nine", "nine9nine")
+    end)
+    |> Enum.map(fn x -> String.graphemes(x) |> Enum.filter(&Aoc2023.First.is_numeric?/1) end)
+    |> Enum.map(fn x -> String.to_integer("#{List.first(x)}#{List.last(x)}") end)
+    |> Enum.reduce(&(&1 + &2))
+  end
+
+  def is_numeric?(str) do
+    Regex.match?(~r/^\d+$/, str)
+  end
+
+  defp get_challenge do
+    {:ok, challenge} = Aoc2023.read_challenge("first")
+
+    challenge
+  end
+end
